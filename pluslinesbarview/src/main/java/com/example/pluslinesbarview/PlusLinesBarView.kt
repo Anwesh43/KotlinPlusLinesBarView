6995229635202c0b38b8cab152e51ad4cf33433c
Anwesh43 <anweshthecool0@gmail.com>
2018-05-08 09:59:04 +0530

    add PlusLinesBarView class

--- pluslinesbarview/src/main/java/com/example/pluslinesbarview/PlusLinesBarView.kt
+++ pluslinesbarview/src/main/java/com/example/pluslinesbarview/PlusLinesBarView.kt
@@ -3,3 +3,26 @@ package com.example.pluslinesbarview
 /**
  * Created by anweshmishra on 08/05/18.
  */
+
+import android.view.*
+import android.content.*
+import android.graphics.*
+
+class PlusLinesBarView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
