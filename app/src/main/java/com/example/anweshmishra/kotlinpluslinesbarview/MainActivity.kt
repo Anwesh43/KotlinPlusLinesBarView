7466dc8ab7e5a3efcaa7306cc446d53c71df1d1a
Anwesh43 <anweshthecool0@gmail.com>
2018-05-08 10:40:41 +0530

    adding view in activity

--- app/src/main/java/com/example/anweshmishra/kotlinpluslinesbarview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinpluslinesbarview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anweshmishra.kotlinpluslinesbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pluslinesbarview.PlusLinesBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PlusLinesBarView.create(this)
     }
 }
