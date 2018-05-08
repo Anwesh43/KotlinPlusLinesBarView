9a8f4f5d9a6fa952f9472cfc951ada88eaa26938
Anwesh43 <anweshthecool0@gmail.com>
2018-05-08 10:45:41 +0530

    adding fullScreen mode

--- app/src/main/java/com/example/anweshmishra/kotlinpluslinesbarview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinpluslinesbarview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anweshmishra.kotlinpluslinesbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.pluslinesbarview.PlusLinesBarView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PlusLinesBarView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 }
